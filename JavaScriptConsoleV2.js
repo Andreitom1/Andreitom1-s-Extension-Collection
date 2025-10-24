9a54d200b0b2321d8b4ab67206652b14ff3d2391
Andrei Tom <andrei.sgd@gmail.com>
2025-10-24 18:55:13 +0300

    New extension :)

---
+++ JavaScriptConsoleV2.js
@@ -0,0 +1,262 @@
+/*
+   This extension was made with TurboBuilder!
+   https://turbobuilder-steel.vercel.app/
+*/
+(async function(Scratch) {
+    const variables = {};
+    const blocks = [];
+    const menus = {};
+
+
+    if (!Scratch.extensions.unsandboxed) {
+        alert("This extension needs to be unsandboxed to run!")
+        return
+    }
+
+    function doSound(ab, cd, runtime) {
+        const audioEngine = runtime.audioEngine;
+
+        const fetchAsArrayBufferWithTimeout = (url) =>
+            new Promise((resolve, reject) => {
+                const xhr = new XMLHttpRequest();
+                let timeout = setTimeout(() => {
+                    xhr.abort();
+                    reject(new Error("Timed out"));
+                }, 5000);
+                xhr.onload = () => {
+                    clearTimeout(timeout);
+                    if (xhr.status === 200) {
+                        resolve(xhr.response);
+                    } else {
+                        reject(new Error(`HTTP error ${xhr.status} while fetching ${url}`));
+                    }
+                };
+                xhr.onerror = () => {
+                    clearTimeout(timeout);
+                    reject(new Error(`Failed to request ${url}`));
+                };
+                xhr.responseType = "arraybuffer";
+                xhr.open("GET", url);
+                xhr.send();
+            });
+
+        const soundPlayerCache = new Map();
+
+        const decodeSoundPlayer = async (url) => {
+            const cached = soundPlayerCache.get(url);
+            if (cached) {
+                if (cached.sound) {
+                    return cached.sound;
+                }
+                throw cached.error;
+            }
+
+            try {
+                const arrayBuffer = await fetchAsArrayBufferWithTimeout(url);
+                const soundPlayer = await audioEngine.decodeSoundPlayer({
+                    data: {
+                        buffer: arrayBuffer,
+                    },
+                });
+                soundPlayerCache.set(url, {
+                    sound: soundPlayer,
+                    error: null,
+                });
+                return soundPlayer;
+            } catch (e) {
+                soundPlayerCache.set(url, {
+                    sound: null,
+                    error: e,
+                });
+                throw e;
+            }
+        };
+
+        const playWithAudioEngine = async (url, target) => {
+            const soundBank = target.sprite.soundBank;
+
+            let soundPlayer;
+            try {
+                const originalSoundPlayer = await decodeSoundPlayer(url);
+                soundPlayer = originalSoundPlayer.take();
+            } catch (e) {
+                console.warn(
+                    "Could not fetch audio; falling back to primitive approach",
+                    e
+                );
+                return false;
+            }
+
+            soundBank.addSoundPlayer(soundPlayer);
+            await soundBank.playSound(target, soundPlayer.id);
+
+            delete soundBank.soundPlayers[soundPlayer.id];
+            soundBank.playerTargets.delete(soundPlayer.id);
+            soundBank.soundEffects.delete(soundPlayer.id);
+
+            return true;
+        };
+
+        const playWithAudioElement = (url, target) =>
+            new Promise((resolve, reject) => {
+                const mediaElement = new Audio(url);
+
+                mediaElement.volume = target.volume / 100;
+
+                mediaElement.onended = () => {
+                    resolve();
+                };
+                mediaElement
+                    .play()
+                    .then(() => {
+                        // Wait for onended
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
+            });
+
+        const playSound = async (url, target) => {
+            try {
+                if (!(await Scratch.canFetch(url))) {
+                    throw new Error(`Permission to fetch ${url} denied`);
+                }
+
+                const success = await playWithAudioEngine(url, target);
+                if (!success) {
+                    return await playWithAudioElement(url, target);
+                }
+            } catch (e) {
+                console.warn(`All attempts to play ${url} failed`, e);
+            }
+        };
+
+        playSound(ab, cd)
+    }
+    class Extension {
+        getInfo() {
+            return {
+                "id": "JSConsoleV2AT1",
+                "name": "JavaScript Console V2",
+                "color1": "#0044cc",
+                "color2": "#001f7a",
+                "blocks": blocks,
+                "menus": menus
+            }
+        }
+    }
+    blocks.push({
+        opcode: "clear",
+        blockType: Scratch.BlockType.COMMAND,
+        text: "clear the console",
+        arguments: {},
+        disableMonitor: true,
+        isEdgeActivated: false
+    });
+    Extension.prototype["clear"] = async (args, util) => {
+        console.clear();
+    };
+
+    blocks.push({
+        opcode: "log",
+        blockType: Scratch.BlockType.COMMAND,
+        text: "console[what][howMany][input]",
+        arguments: {
+            "what": {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'menu1'
+            },
+            "howMany": {
+                type: Scratch.ArgumentType.STRING,
+                menu: 'menu2'
+            },
+            "input": {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'Hello World!',
+            },
+        },
+        disableMonitor: true,
+        isEdgeActivated: false
+    });
+    Extension.prototype["log"] = async (args, util) => {
+        if (Boolean((args["howMany"] == 'single'))) {
+            switch (args["what"]) {
+                case ('log'):
+                    console.log(args["input"]);
+                    break;
+                case ('warn'):
+                    console.warn(args["input"]);
+                    break;
+                case ('error'):
+                    console.error(args["input"]);
+                    break;
+                case ('count'):
+                    console.count(args["input"]);
+                    break;
+            };
+
+        } else {
+            for (let i = 0; i < JSON.parse(args["input"]).length; i++) {
+                switch (args["what"]) {
+                    case ('log'):
+                        console.log(JSON.parse(args["input"])[i]);
+                        break;
+                    case ('warn'):
+                        console.warn(JSON.parse(args["input"])[i]);
+                        break;
+                    case ('error'):
+                        console.error(JSON.parse(args["input"])[i]);
+                        break;
+                    case ('count'):
+                        console.count(JSON.parse(args["input"])[i]);
+                        break;
+                }
+            };
+
+        };
+    };
+
+    blocks.push({
+        opcode: "logMore",
+        blockType: Scratch.BlockType.COMMAND,
+        text: "print more with different types[input]",
+        arguments: {
+            "input": {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: '[[\"a\", \"log\"],[\"b\", \"warn\"],[\"c\", \"error\"]]',
+            },
+        },
+        disableMonitor: true,
+        isEdgeActivated: false
+    });
+    Extension.prototype["logMore"] = async (args, util) => {
+        for (let i = 0; i < JSON.parse(args["input"]).length; i++) {
+            switch (JSON.parse(args["input"])[i][1]) {
+                case ('log'):
+                    console.log(JSON.parse(args["input"])[i][0]);
+                    break;
+                case ('warn'):
+                    console.warn(JSON.parse(args["input"])[i][0]);
+                    break;
+                case ('error'):
+                    console.error(JSON.parse(args["input"])[i][0]);
+                    break;
+                case ('count'):
+                    console.count(JSON.parse(args["input"])[i][0]);
+                    break;
+            }
+        };
+    };
+
+    menus["menu1"] = {
+        acceptReporters: true,
+        items: ['log', 'warn', 'error', 'count']
+    }
+
+    menus["menu2"] = {
+        acceptReporters: false,
+        items: ['single', 'multiple (array)']
+    }
+
+    Scratch.extensions.register(new Extension());
+})(Scratch);
